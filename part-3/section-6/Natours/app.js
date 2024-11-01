517e017d144b977bacb0519ae852e7eacec4b7f7
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-11-01 09:27:36 +0100

    feat: add get and post request

--- part-3/section-6/Natours/app.js
+++ part-3/section-6/Natours/app.js
@@ -5,14 +5,81 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// GET TOURS
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint...');
+// GET A SPECIFIC TOUR
+app.get('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID!',
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+});
+
+// CREATE A NEW TOUR
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'Could not create the new tour!',
+        });
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
