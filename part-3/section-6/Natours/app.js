70c7a556e18bf3418e6c738db50d4c3577b5ea3e
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-11-01 09:26:31 +0100

    feat: add code base for section 6

---
+++ part-3/section-6/Natours/app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
