21b7489859461e190d36654973ca2ec4cfa905b1
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-11-05 06:07:40 +0100

    feat: add middleware

--- part-3/section-6/Natours/app.js
+++ part-3/section-6/Natours/app.js
@@ -5,6 +5,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from middleware!!!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -12,8 +22,10 @@ const tours = JSON.parse(
 // ROUTES
 // GET ALL TOURS
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
