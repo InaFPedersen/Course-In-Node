2d1a5301160bd697203e106503d8b0fb91e03a94
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-11-08 12:00:12 +0100

    feat: add routes

---
+++ part-3/section-7/Natours/routes/tourRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const tourController = require('./../controllers/tourController');
+
+// ROUTES
+const router = express.Router();
+
+router.param('id', tourController.checkID);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createANewTour);
+
+router
+  .route('/:id')
+  .get(tourController.getSpecificTour)
+  .patch(tourController.updateASpecificTour)
+  .delete(tourController.deleteASpecificTour);
+
+module.exports = router;
