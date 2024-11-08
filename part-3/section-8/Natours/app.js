ee3ed909fa9b4e606382c91bc09b37ca78b4133f
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-11-08 12:58:48 +0100

    feat: add code base for section 8

---
+++ part-3/section-8/Natours/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware!!!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// MOUNT ROUTERS
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// ROUTE HANDLERS
+// app.get('/api/v1/tours', getAllTours); // GET ALL TOURS
+// app.get('/api/v1/tours/:id', getSpecificTour); // GET A SPECIFIC TOUR
+// app.post('/api/v1/tours', createANewTour); // CREATE A NEW TOUR
+// app.patch('/api/v1/tours/:id', updateASpecificTour); // UPDATE A SPECIFIC TOUR
+// app.delete('/api/v1/tours/:id', deleteASpecificTour); // DELETE A SPECIFIC TOUR
+
+module.exports = app;
